e0ee084b9b4a9c48f7015e00f0250bb3d7f1a88c
jeresc <jeremiastomassrc@gmail.com>
2023-06-02 16:07:02 -0300

    Adding callback examples and callback script to npm

---
+++ src/callback/index.js
@@ -0,0 +1,19 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+  return callback(num1, num2);
+}
+
+console.log(calc(2, 8, sum));
+
+setTimeout(function () {
+  console.log("Hello JavaScript");
+}, 5000);
+
+function gretting(name) {
+  console.log(`Hello ${name}!`);
+}
+
+setTimeout(gretting, 2000, "Jeremias");
